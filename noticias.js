fda58294d75b515ef146aab8b135a49cf0dfc2e2
Jesuarinconero <jvergaragonzalez@safareyes.es>
2023-03-08 08:42:50 +0100

    Actualizador de noticias

--- noticias.js
+++ noticias.js
@@ -1,10 +1,27 @@
-function iniciar(){
-    const datos = JSON.parse(localStorage.getItem("datos"));
+function iniciar() {
     const contenedor = document.getElementById("datos");
-
-    for (let propiedad in datos) {
-        const elemento = document.createElement("p");
-        elemento.innerHTML = `${propiedad}: ${datos[propiedad]}`;
-        contenedor.appendChild(elemento);
+    for (let i = 0; i < localStorage.length; i++) {
+        const clave = localStorage.key(i);
+        if (clave.startsWith("datos-")) { // Buscar entradas con prefijo "datos-"
+            const datosJSON = localStorage.getItem(clave);
+            const datos = JSON.parse(datosJSON);
+            const elementoExistente = contenedor.querySelector(`div[data-identificador="${clave}"]`);
+            if (elementoExistente) { // Si ya existe, actualizar sus datos
+                for (let propiedad in datos) {
+                    elementoExistente.querySelector(`p[data-propiedad="${propiedad}"]`).innerHTML = `${propiedad}: ${datos[propiedad]}`;
+                }
+            } else { // Si no existe, crear uno nuevo
+                const nuevoElemento = document.createElement("div");
+                nuevoElemento.setAttribute("data-identificador", clave);
+                for (let propiedad in datos) {
+                    const elemento = document.createElement("p");
+                    elemento.setAttribute("data-propiedad", propiedad);
+                    elemento.innerHTML = `${propiedad}: ${datos[propiedad]}`;
+                    nuevoElemento.appendChild(elemento);
+                }
+                contenedor.appendChild(nuevoElemento);
+            }
+        }
     }
 }
+
